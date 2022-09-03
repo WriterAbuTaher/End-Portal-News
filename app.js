bd155e7954cd8872efb115666b1dccae12eb8546
Abu Taher <writerabutaher@gmail.com>
2022-09-03 13:51:09 +0600

    news section added

--- app.js
+++ app.js
@@ -34,6 +34,39 @@ const loadNews = (id) => {
 const displayNews = (newses) => {
     newses.forEach(news => {
         console.log(news);
+
+        const cutText = (news.details).slice(1, 300);
+
+        const newsContainer = document.getElementById("news-container");
+
+        const div = document.createElement("div")
+
+        div.innerHTML = `
+        <div class="card mb-3 w-75 mx-auto">
+            <div class="row g-0">
+                <div class="col-md-4 col-lg-2">
+                    <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 col-lg-10">
+                    <div class="card-body">
+                        <h5 class="mt-0">${news.title}</h5>
+                        <p>${cutText}...</p>
+                    </div>
+                        <div class="card-footer">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="d-flex">
+                                    <img class="rounded-5" width="30" height="25" src="${news.author.img}" alt="">
+                                    <p class="fw-bold mx-2">${news.author.name === null || news.author.name === "system" ? news.author.name = "Not Found" : news.author.name}</p>
+                                </div>
+                            <p>${news.total_view === null ? news.total_view = "Not Found" : news.total_view}👁‍🗨</p>
+                            <button href="#" class="btn btn-success">Read details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        newsContainer.appendChild(div)
     })
 }
 
